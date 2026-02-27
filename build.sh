18809bd381e6888dc098db029f9a9b438955086e
Vikas Pal <9368vikas@gmail.com>
2026-02-27 22:09:28 +0530

    Fix Vercel build: pass --break-system-packages to pip

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Building project..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 
 echo "Make Migration..."
 python3 ecommerce/manage.py makemigrations
