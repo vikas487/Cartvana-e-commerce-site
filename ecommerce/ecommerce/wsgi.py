19389f10da48795102b77716b472e8db67b99686
Vikas Pal <9368vikas@gmail.com>
2026-02-27 22:17:48 +0530

    Revert Vercel deployment config

--- ecommerce/ecommerce/wsgi.py
+++ ecommerce/ecommerce/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
